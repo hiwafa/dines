59f7c0baf5ed2cd95dc0c3cd6d8e3427e5a6860b
Ahmad <joya.abn@gmail.com>
2021-09-28 10:54:49 +0430

    updates

--- config/database.js
+++ config/database.js
@@ -1,23 +1,24 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "dines"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "ajoajo2245"),
-        ssl: env.bool("DATABASE_SSL", false),
-        ssl: {
-          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+const { parse } = require("pg-connection-string");
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: { rejectUnauthorized: false },
+          options: {
+            ssl: false,
+          },
         },
       },
-      options: {
-        ssl: true,
-      },
     },
-  },
-});
+  };
+};
