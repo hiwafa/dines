3639f62ba61bf6b4fb4c1e1f08df59177d478ea6
hekmatnajafi <najafihekmat@gmail.com>
2021-10-22 15:34:18 +0430

    updated food

--- api/foods/controllers/foods.js
+++ api/foods/controllers/foods.js
@@ -12,5 +12,32 @@ module.exports = {
         let populate;
         populate = ["comments", "categories", "ingredients", "images", "comments.created_by", "comments.reply"]
         return await strapi.services.foods.find({ restaurant }, populate)
+    },
+
+    async update(ctx) {
+        let { ingredients } = ctx.request.body;
+        let food = ctx.request.body;
+        let ingreds = [];
+
+        for (let i = 0; i < ingredients.length; i++) {
+            const exist = await strapi.services.ingredients.findOne({ name: ingredients[i] })
+            if (exist?.id) {
+                ingreds.push(exist.id);
+            }
+            else {
+                const add = await strapi.services.ingredients.create({ name: ingredients[i] })
+                ingreds.push(add.id)
+            }
+        }
+        food.ingredients = ingreds
+
+        // update db
+        try {
+            return await strapi.services.foods.update({ id: food.id }, food)
+        }
+        catch (e) {
+            return e;
+        }
+
     }
 };
